fa99539b0ed4bf21ccff88c261aa72f8b4fbd079
Mianbao <gll004@ucsd.edu>
2026-05-09 09:59:10 -0700

    Add toggle button

--- extension/popup.js
+++ extension/popup.js
@@ -1,13 +1,23 @@
-document.getElementById("check").addEventListener("click", async () => {
-    const result = document.getElementById("result");
-    result.innerText = "Analyzing...";
+document.addEventListener('DOMContentLoaded', () => {
+    // TOGGLE SWITCH LOGIC
+    document.getElementById("switch").addEventListener("change", async function () {
+        const heading = document.getElementById("heading");
+        const result = document.getElementById("result");
 
-    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-    chrome.tabs.sendMessage(tab.id, { action: "getEmail" }, async ({ text }) => {
-        if (!text) {
-            result.innerText = "No email found. Open an email in Gmail first.";
-            return;
+        // Switch is "ON", start detecting email content logic:
+        if (this.checked) {
+            heading.textContent = "Detecting...";
+            
+            const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+            chrome.tabs.sendMessage(tab.id, { action: "getEmail" }, async ({ text }) => {
+                if (!text) {
+                    result.innerText = "No email found. Open an email in Gmail first.";
+                    return;
+                }
+            });
+        } else {
+            heading.textContent = "Start detection:";
+            result.innerText = "";
         }
-    })
-})
+    });
+});
